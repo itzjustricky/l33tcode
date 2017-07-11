aed600de63ba18dc33c70eb359b93fb6c0945f0e
itzjustricky <chang.ricky08@gmail.com>
2017-07-11 10:27:43 -0400

    still there is a small logic bug on q05; shouldn't be hard to fix

--- code/q05_longest_palindrome_string.cpp
+++ code/q05_longest_palindrome_string.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 struct int_pair_hash {
     size_t operator() (const pair<int, int>& intPair) const {
+        // since string will be maximum of size 1000
         return intPair.first + intPair.second * 1000;
     }
 };
@@ -32,39 +33,41 @@ public:
         return (got != tracker.end());
     }
 
-    int calcLongestPalindrome(string s, const int_pair& section, pair2int_map& longestTracker) {
+    int calcLongestPalindrome(string s, const int_pair& section, int_pair& longestSoFar,
+                              pair2int_map& longestTracker, int carry=0) {
         int start = section.first,
             end = section.second;
-        // cout << start << "," << end << endl;
-        if ((end-start) == 0) {
-            return 1;
-        } else if ((end-start) == 1) {
-            if (s[start] == s[end]) {
-                return 2;
-            } else {
-                return 0;
-            }
+        int start_to_end = end - start;
+        if (start_to_end == 0) {
+            longestTracker[section] = carry + 1;
+        } else if ((start_to_end == 1) &&
+                   (s[start] == s[end])) {
+            longestTracker[section] = carry + 2;
         }
 
         if (pairInMap(section, longestTracker)) { return longestTracker[section]; }
         if (s[start] == s[end]) {
             int_pair newSection = make_pair(start+1, end-1);
             longestTracker[section] = \
-                calcLongestPalindrome(s, newSection, longestTracker) \
-                + 2;
+                calcLongestPalindrome(s, newSection, longestSoFar, longestTracker, carry+2);
         } else {
             const int_pair& newSection1 = make_pair(start+1, end),
                             newSection2 = make_pair(start, end-1);
             longestTracker[section] = \
-                max(calcLongestPalindrome(s, newSection1, longestTracker),
-                    calcLongestPalindrome(s, newSection2, longestTracker));
+                max(calcLongestPalindrome(s, newSection1, longestSoFar, longestTracker),
+                    calcLongestPalindrome(s, newSection2, longestSoFar, longestTracker));
         }
 
+        if (longestTracker[section] > longestTracker[longestSoFar]) {
+            cout << "The substring " << s.substr(longestSoFar.first, longestSoFar.second+1) \
+                 << " is now the longest substring" << endl;
+
+            longestSoFar = section;
+        }
         return longestTracker[section];
     }
 
-    // string longestPalindrome(string s) {
-    int longestPalindrome(string s) {
+    string longestPalindrome(string s) {
         size_t n = s.length();
         if (n == 0) {
             return 0;
@@ -75,8 +78,10 @@ public:
         longestTracker.reserve(n * n);
 
         int_pair wholeSection = make_pair(0, n);
-        int longestLength = calcLongestPalindrome(s, wholeSection, longestTracker);
-        return longestLength;
+        int_pair longestSoFar = make_pair(0, 0);
+        calcLongestPalindrome(s, wholeSection, longestSoFar, longestTracker);
+
+        return s.substr(longestSoFar.first, longestSoFar.second+1);
     }
 
 };
@@ -84,9 +89,11 @@ public:
 
 int main() {
     Solution sol;
-    string s = "abcdefghijka";
-    int longestLength = sol.longestPalindrome(s);
-    cout << "The longest length is: " << longestLength << endl;
+    string s = "afblalbacbdefaaaaaaaabbbbbbaaaaaaaaaa";
+    string correctAnswer = "aaaaaaaabbbbbbaaaaaaaa";
+    string longestPalindrome = sol.longestPalindrome(s);
+    cout << "The longest length is: " << longestPalindrome << endl;
+    cout << "The correct answer is: " << correctAnswer << endl;
 
     return 0;
 }
