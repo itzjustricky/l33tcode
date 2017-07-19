9722cd38a2de33864bde9e8f4f2cb2135cf9ed6b
itzjustricky <chang.ricky08@gmail.com>
2017-07-19 00:01:42 -0400

    had bad assumption, must correct for that

--- code/q501_find_mode_in_bst.cpp
+++ code/q501_find_mode_in_bst.cpp
@@ -18,6 +18,14 @@
 using namespace std;
 
 
+void printVector(const vector<int>& v) {
+    for (auto x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -30,7 +38,7 @@ class Solution {
 public:
 
     // first stores count, second stores numbers with that count
-    typedef pair<int, vector<int>> mode_count;
+    typedef pair<int, vector<int>> mode_pair;
 
     bool isLeaf(TreeNode* node) {
         return ((node->left == NULL) && (node->right == NULL));
@@ -40,76 +48,96 @@ public:
         return find(v.begin(), v.end(), x) != v.end();
     }
 
-    void updateModeWithValue(int rootVal, mode_count& leftSubTree) {
-        if (leftSubTree.first == 1) {
-            leftSubTree.second.emplace_back(rootVal);
-        } else if (isInVector(rootVal, leftSubTree.second)) {
+    void updateMode(int rootVal, mode_pair& leftSubTree) {
+        if (isInVector(rootVal, leftSubTree.second)) {
             leftSubTree.first += 1;
-            leftSubTree.second = vector<int>(rootVal);
+            leftSubTree.second = vector<int>(1, rootVal);
+        } else if (leftSubTree.first == 0) {
+            leftSubTree.first = 1;
+            leftSubTree.second = vector<int>(1, rootVal);
+        } else if (leftSubTree.first == 1) {
+            leftSubTree.second.push_back(rootVal);
         }
-
     }
 
     vector<int> combineVectors(const vector<int>& v1, const vector<int>& v2) {
-        vector<int> newVector(v1.size() + v2.size());
+        vector<int> newVector;
         newVector.insert(newVector.end(), v1.begin(), v1.end());
         newVector.insert(newVector.end(), v2.begin(), v2.end());
         return newVector;
     }
 
-    vector<int> accumulateModes(const vector<int>& left_modes, const vector<int>& right_modes) {
-        vector<int> accmModes;
-
-        for (auto x : left_modes) {
-            if (isInVector(x, right_modes)) {
-                accmModes.push_back(x);
+    mode_pair intersectModes(const mode_pair& left_modes, const mode_pair& right_modes) {
+        vector<int> modeIntersect;
+        for (auto x : left_modes.second) {
+            if (isInVector(x, right_modes.second)) {
+                modeIntersect.push_back(x);
             }
         }
-        return accmModes;
+
+        if (modeIntersect.size() > 0) {
+            return make_pair(left_modes.first + right_modes.first, modeIntersect);
+        } else {
+            return make_pair(0, modeIntersect);
+        }
     }
 
-    mode_count recursiveFindMode(TreeNode* root) {
+    mode_pair recursiveFindMode(TreeNode* root) {
+        if (root == NULL) {
+            return make_pair(0, vector<int>());
+        }
         if (isLeaf(root)) {
             return make_pair(1, vector<int>(1, root->val));
         }
 
-        mode_count leftSubTree = recursiveFindMode(root->left);
-        mode_count rightSubTree = recursiveFindMode(root->right);
-        vector<int> accmModes = accumulateModes(leftSubTree.second, rightSubTree.second);
+        vector<int> mergedModes;
 
-        if (accmModes.size() > 0) {
-            if (isInVector(root->val, accmModes)) {
-                return make_pair(leftSubTree.first + rightSubTree.first + 1,
-                                 vector<int>(1, root->val));
-            } else {
-                return make_pair(leftSubTree.first + rightSubTree.first,
-                                 accmModes);
-            }
+        mode_pair leftSubTree = recursiveFindMode(root->left);
+        mode_pair rightSubTree = recursiveFindMode(root->right);
+        // here I count the root as part of the left sub-tree
+        updateMode(root->val, leftSubTree);
+
+        mode_pair subTreeIntersect = intersectModes(leftSubTree, rightSubTree);
+        if (subTreeIntersect.first > 0) {
+            return subTreeIntersect;
+
         } else {
-            // TODO: need to handle the root value too
-            updateModeWithValue(root->val, leftSubTree);
-
             if (leftSubTree.first > rightSubTree.first) {
                 return leftSubTree;
             } else if (leftSubTree.first < rightSubTree.first) {
                 return rightSubTree;
-            } else {
-                return make_pair(leftSubTree.first,
-                                 combineVectors(leftSubTree.second, rightSubTree.second));
+            } else {    // mode count of left and right are the same
+                mergedModes = combineVectors(leftSubTree.second, rightSubTree.second);
+                return make_pair(leftSubTree.first, mergedModes);
             }
         }
 
     }
 
     vector<int> findMode(TreeNode* root) {
-        mode_count treeModes = recursiveFindMode(root);
-
+        mode_pair treeModes = recursiveFindMode(root);
         return treeModes.second;
     }
 };
 
-int main() {
 
+TreeNode* constructTree() {
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    // root->right->left = new TreeNode(2);
+
+    return root;
+}
+
+
+int main() {
+    TreeNode* tree = constructTree();
+
+    Solution sol;
+    vector<int> modes = sol.findMode(tree);
+
+    cout << "The modes are: " << endl;
+    printVector(modes);
 
     return 0;
 }
