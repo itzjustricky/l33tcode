338cc2226f9f224b045abe3839892ec46ae128d6
itzjustricky <chang.ricky08@gmail.com>
2017-08-01 08:28:21 -0400

    made small change, is now 9ms answer

--- code/q576_out_of_boundary_paths.cpp
+++ code/q576_out_of_boundary_paths.cpp
@@ -18,9 +18,10 @@ private:
     const static int MAX_HEIGHT = 50,
                      MAX_WIDTH = 50,
                      MAX_MOVES = 50;
-    const static unsigned int ANSWER_MOD = pow(10, 9) + 7;
+    const unsigned int ANSWER_MOD = pow(10, 9) + 7;
 
     int m_GridHash[MAX_HEIGHT][MAX_WIDTH][MAX_MOVES] = {{{0}}};
+    bool m_GridHashIndicator[MAX_HEIGHT][MAX_WIDTH][MAX_MOVES] = {{{false}}};
     int m_GridHeight;
     int m_GridWidth;
 
@@ -41,7 +42,7 @@ private:
             return 1;
         } else if (N == 0) {
             return 0;
-        } else if (m_GridHash[i][j][N-1] != -1) {
+        } else if (m_GridHashIndicator[i][j][N-1]) {
             return m_GridHash[i][j][N-1];
         }
 
@@ -50,25 +51,14 @@ private:
              exploreGridPaths(i+1, j, N-1) + \
              exploreGridPaths(i, j-1, N-1) + \
              exploreGridPaths(i, j+1, N-1)) % ANSWER_MOD;
+        m_GridHashIndicator[i][j][N-1] = true;
         return m_GridHash[i][j][N-1];
-    }
-
-    void initializeHashValue(int val) {
-        for (int i = 0; i < MAX_HEIGHT; ++i) {
-            for (int j = 0; j < MAX_WIDTH; ++j) {
-                for (int k = 0; k < MAX_MOVES; ++k) {
-                    m_GridHash[i][j][k] = val;
-                }
-            }
-        }
     }
 
 public:
 
     int findPaths(int m, int n, int N, int i, int j) {
         m_GridHeight = m; m_GridWidth = n;
-        initializeHashValue(-1);
-
         return exploreGridPaths(i, j, N);
     }
 };
