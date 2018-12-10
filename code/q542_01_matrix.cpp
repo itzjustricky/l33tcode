2cd25e8dc295415b2654ac3521db7cc991b455fb
itzjustricky <chang.ricky08@gmail.com>
2018-12-09 23:34:16 -0500

    started question 542 and 717

---
+++ code/q542_01_matrix.cpp
@@ -0,0 +1,58 @@
+/*
+ * Question 542
+
+    Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+
+    The distance between two adjacent cells is 1.
+    Example 1:
+        Input:
+        0 0 0
+        0 1 0
+        0 0 0
+        Output:
+        0 0 0
+        0 1 0
+        0 0 0
+
+    Example 2:
+        Input:
+        0 0 0
+        0 1 0
+        1 1 1
+        Output:
+        0 0 0
+        0 1 0
+        1 2 1
+
+    Note:
+        1. The number of elements of the given matrix will not exceed 10,000.
+        2. There are at least one 0 in the given matrix.
+        3. The cells are adjacent in only four directions: up, down, left and right.
+ *
+ *
+*/
+
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size(),
+            m = matrix[0].size();
+
+
+    }
+};
+
+
+int main() {
+
+    Solution sol;
+    // sol.updateMatrix();
+
+    return 0;
+}
