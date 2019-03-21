1fe16c32c4a74990d6bf1ff7cd60bb1a9f36fcbd
itzjustricky <chang.ricky08@gmail.com>
2019-03-21 00:44:00 -0400

    miscellaneous dir for random questions

---
+++ misc/cpp/efficient_cost.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+    private:
+        vector<int> initCostMap() {
+
+        }
+
+    public:
+        int calculateCost(vector<int> a, int k) {
+
+        }
+}
+
+
+
+int main() {
+
+    return 0;
+}
