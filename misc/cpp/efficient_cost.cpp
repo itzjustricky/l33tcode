1068c18769c5bad23435c493498acfc1591f3917
itzjustricky <chang.ricky08@gmail.com>
2019-03-22 08:55:07 -0400

    small edit

--- misc/cpp/efficient_cost.cpp
+++ misc/cpp/efficient_cost.cpp
@@ -7,15 +7,25 @@ using namespace std;
 
 class Solution {
     private:
-        vector<int> initCostMap() {
+        vector<vector<int>> initCostMap(const vector<int>& a, int k) {
+            int N = a.size();
+            vector<vector<int>> costMap(k, vector<int>(0, N));
 
+            return costMap;
         }
 
     public:
-        int calculateCost(vector<int> a, int k) {
+        int calculateCost(vector<int>& a, int k) {
+            vector<vector<int>> costMap = initCostMap(a, k);
 
+            int N = a.size();
+            // for index i, calculates the min cost that can be incurred
+            // for subvector spanning from beginning up to index i
+            vector<int> minCost(0, N);
+
+            return minCost[N-1];
         }
-}
+};
 
 
 
