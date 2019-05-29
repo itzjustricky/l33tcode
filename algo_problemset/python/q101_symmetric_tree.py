b819bfe055b01abb4b01c28ed6042be4132692b6
itzjustricky <chang.ricky08@gmail.com>
2019-05-29 13:10:08 -0400

    completed question 101 (symmetric tree) and 226 (invert binary tree)

---
+++ algo_problemset/python/q101_symmetric_tree.py
@@ -0,0 +1,45 @@
+"""
+
+101. Symmetric Tree
+Easy
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    @staticmethod
+    def extractValue(root: TreeNode):
+        if root is None:
+            return None
+        else:
+            return root.val
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+
+        left_queue = [root.left]
+        right_queue = [root.right]
+
+        while (len(left_queue) > 0) and (len(right_queue) > 0):
+
+            left_node = left_queue.pop(0)
+            right_node = right_queue.pop(0)
+            if self.extractValue(left_node) != self.extractValue(right_node):
+                return False
+
+            if left_node is not None:
+                left_queue.extend([left_node.left, left_node.right])
+            if right_node is not None:
+                right_queue.extend([right_node.right, right_node.left])
+
+        if (len(left_queue) > 0) or (len(right_queue) > 0):
+            return False
+        else:
+            return True
---
+++ algo_problemset/python/q226_invert_binary_tree.py
@@ -0,0 +1,26 @@
+"""
+226. Invert Binary Tree
+Easy
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def recursiveInvert(self, node: TreeNode):
+        if node is None:
+            return
+
+        node.left, node.right = node.right, node.left
+        self.recursiveInvert(node.left)
+        self.recursiveInvert(node.right)
+
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        self.recursiveInvert(root)
