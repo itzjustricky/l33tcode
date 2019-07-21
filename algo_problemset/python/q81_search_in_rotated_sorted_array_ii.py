291f2ab93cdc9e2f4bece02b2e871ca61cac1f20
itzjustricky <chang.ricky08@gmail.com>
2019-07-21 18:12:38 -0400

    completed question 81: Search in Rotated Sorted Array II (slow solution)

--- algo_problemset/python/q81_search_in_rotated_sorted_array_ii.py
+++ algo_problemset/python/q81_search_in_rotated_sorted_array_ii.py
@@ -5,14 +5,14 @@ Medium
 """
 
 from typing import List
+from typing import TypeVars
 
 
 class Solution:
 
-    def search(self, nums: List[int], target: int) -> bool:
-        if len(nums) == 0: return False
-
-        lbound, rbound = 0, len(nums)-1
+    def binarySearch(self, nums: List[int], target: int,
+                     start: int, end: int) -> bool:
+        lbound, rbound = start, end
 
         while (rbound - lbound) > 1:
             mid = (rbound + lbound) // 2
@@ -20,14 +20,26 @@ class Solution:
             if target == nums[mid]:
                 return True
             elif target < nums[mid]:
+                # if this is the scenario it can be on either side
+                if (nums[mid] == nums[rbound]):
+                    return self.binarySearch(nums, target, lbound, mid-1) or \
+                           self.binarySearch(nums, target, mid+1, rbound)
                 if (nums[mid] > nums[rbound]) and (target <= nums[rbound]):
                     lbound = mid
                 else:
                     rbound = mid
             else:   # target > nums[mid]
+                if (nums[mid] == nums[lbound]):
+                    return self.binarySearch(nums, target, lbound, mid-1) or \
+                           self.binarySearch(nums, target, mid+1, rbound)
                 if (nums[mid] < nums[lbound]) and (target >= nums[lbound]):
                     rbound = mid
                 else:
                     lbound = mid
 
         return (nums[lbound] == target) or (nums[rbound] == target)
+
+    def search(self, nums: List[int], target: int) -> bool:
+        if len(nums) == 0: return False
+
+        return self.binarySearch(nums, target, 0, len(nums)-1)
